9f7162b0ef347e42728ffd34ce8b5751cdcf12ca
Utkarsh3128 <ukumar21-cse@bvucoep.edu.in>
2023-09-01 22:27:55 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./BgChanger-react",
+  base: "/BgChanger-react",
   plugins: [react()],
 })
